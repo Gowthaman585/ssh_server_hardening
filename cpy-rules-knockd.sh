4e9e201461ce9ce9d8a44644b7b0b11f557e4e84
Gowthaman M <gowthamanm585@gmail.com>
2025-12-16 11:51:09 +0530

    Changed file-name and adjust file rules

---
+++ cpy-rules-knockd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#checking the presence of knockd.service before attempting to copy into etc
+
+knockd_path="/usr/lib/systemd/system/knockd.service"
+target_path="kconfRule.txt"
+
+if [ -f "$knockd_path" ];
+then
+ 	sudo cp "$knockd_path" /etc/knockd.conf
+	sudo cp "$target_path" /etc/knockd.conf
+else
+	echo "error : knockd.service not found"
+	echo "Please enusre that you are downloaded knockd daemon."
+fi
+
