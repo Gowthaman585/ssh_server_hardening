75407edfe31f79f93b374510620b22381065cae4
Gowthaman M <gowthamanm585@gmail.com>
2026-02-06 20:50:03 +0530

    Renamed cpy-rules-knockd.sh to set_knock_service.sh

---
+++ set_knockd_service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#checking the presence of knockd.service before attempting to copy into etc
+
+knockd_path="/usr/lib/systemd/system/knockd.service"
+target_path="kdaemon_rules.txt"
+
+if [ -f "$knockd_path" ];
+then
+ 	sudo cp "$knockd_path" /etc/knockd.conf
+	sudo cp "$target_path" /etc/knockd.conf
+else
+	echo "error : knockd.service not found"
+	echo "Please enusre that you are downloaded knockd daemon."
+	echo "Please install knockd daemon: [apt install knockd]"
+fi
+
